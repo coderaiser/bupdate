cdac7f13f2f467af23539cd49dd3dea495a75af9
coderaiser <mnemonic.enemy@gmail.com>
2015-07-04 11:19:33 -0400

    feature(bupdate) add

---
+++ README.md
@@ -0,0 +1,33 @@
+# Bupdate
+
+Update bower dependecy and change `bower.json` if version is bigger.
+For npm you could use [nupdate](https://coderaiser/nupdate "Nupdate").
+
+Same as:
+
+```sh
+bower install <module> --save && bower uninstall <module> --save
+```
+
+## Install
+
+```
+npm i bupdate -g
+```
+
+## How to use?
+
+```sh
+bupdate jquery
+```
+
+### Options
+
+```
+-v, --version   - show version number and exit
+-h, --help      - show help and exit
+```
+
+## License
+
+MIT
---
+++ bin/bupdate.js
@@ -0,0 +1,68 @@
+(function() {
+    'use strict';
+    
+    var argv    = process.argv.slice(2),
+        option  = argv[0];
+    
+    if (!option || /^(-h|--help)$/.test(option))
+        help();
+    
+    else if (/^(-v|--version)$/.test(option))
+        version();
+    
+    else 
+        bupdate(option);
+        
+    function bupdate(name) {
+        var bower   = require('bower'),
+            async   = require('async');
+        
+        async.waterfall([
+            function uninstall(callback) {
+                bower.commands
+                    .uninstall([name], { save: true })
+                    .on('error', callback)
+                    .on('end', function(data) {
+                        callback(null, data);
+                    });
+            },
+            
+            function logUninstall(data, callback) {
+                callback();
+            },
+            
+            function install(callback) {
+                bower.commands
+                    .install([name], { save: true })
+                    .on('error', callback)
+                    .on('end', function(data) {
+                        callback(null, data);
+                    });
+            },
+            
+            function logInstall(data, callback) {
+                console.log(name, 'v' + data[name].pkgMeta.version);
+                callback();
+            },
+        ], function(e) {
+            if (e)
+                console.error(e.message);
+        });
+    }
+        
+    function help() {
+        var bin         = require('../json/bin'),
+            usage       = 'Usage: bupdate [options]';
+        
+        console.log(usage);
+        console.log('Options:');
+        
+        Object.keys(bin).forEach(function(name) {
+            console.log('  %s %s', name, bin[name]);
+        });
+    }
+    
+    function version() {
+        console.log('v' + require('../package'));
+    }
+})();
