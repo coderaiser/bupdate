cb22ae784f32d5bf63656df48849ca804549d060
coderaiser <mnemonic.enemy@gmail.com>
2016-05-23 20:49:51 +0300

    feature(bupdate) add -E, --save-exact

--- bin/bupdate.js
+++ bin/bupdate.js
@@ -1,70 +1,72 @@
 #!/usr/bin/env node
 
-(function() {
-    'use strict';
+'use strict';
+
+var argv    = process.argv.slice(2);
+var option  = argv[0];
+var saveExact;
+
+if (!option || /^(-h|--help)$/.test(option))
+    help();
+else if (/^(-v|--version)$/.test(option))
+    version();
+else if (/^(-E|--save-exact)$/.test(option))
+    saveExact = true;
+else 
+    bupdate(option);
     
-    var argv    = process.argv.slice(2),
-        option  = argv[0];
+function bupdate(name) {
+    var bower   = require('bower'),
+        async   = require('async');
     
-    if (!option || /^(-h|--help)$/.test(option))
-        help();
+    async.waterfall([
+        function uninstall(callback) {
+            bower.commands
+                .uninstall([name], { save: true })
+                .on('error', callback)
+                .on('end', function(data) {
+                    callback(null, data);
+                });
+        },
+        
+        function logUninstall(data, callback) {
+            callback();
+        },
+        
+        function install(callback) {
+            bower.commands
+                .install([name], {
+                    save: true,
+                    saveExact: saveExact
+                })
+                .on('error', callback)
+                .on('end', function(data) {
+                    callback(null, data);
+                });
+        },
+        
+        function logInstall(data, callback) {
+            console.log(name, 'v' + data[name].pkgMeta.version);
+            callback();
+        },
+    ], function(e) {
+        if (e)
+            console.error(e.message);
+    });
+}
     
-    else if (/^(-v|--version)$/.test(option))
-        version();
+function help() {
+    var bin         = require('../json/bin'),
+        usage       = 'Usage: bupdate [options]';
     
-    else 
-        bupdate(option);
-        
-    function bupdate(name) {
-        var bower   = require('bower'),
-            async   = require('async');
-        
-        async.waterfall([
-            function uninstall(callback) {
-                bower.commands
-                    .uninstall([name], { save: true })
-                    .on('error', callback)
-                    .on('end', function(data) {
-                        callback(null, data);
-                    });
-            },
-            
-            function logUninstall(data, callback) {
-                callback();
-            },
-            
-            function install(callback) {
-                bower.commands
-                    .install([name], { save: true })
-                    .on('error', callback)
-                    .on('end', function(data) {
-                        callback(null, data);
-                    });
-            },
-            
-            function logInstall(data, callback) {
-                console.log(name, 'v' + data[name].pkgMeta.version);
-                callback();
-            },
-        ], function(e) {
-            if (e)
-                console.error(e.message);
-        });
-    }
-        
-    function help() {
-        var bin         = require('../json/bin'),
-            usage       = 'Usage: bupdate [options]';
-        
-        console.log(usage);
-        console.log('Options:');
-        
-        Object.keys(bin).forEach(function(name) {
-            console.log('  %s %s', name, bin[name]);
-        });
-    }
+    console.log(usage);
+    console.log('Options:');
     
-    function version() {
-        console.log('v' + require('../package').version);
-    }
-})();
+    Object.keys(bin).forEach(function(name) {
+        console.log('  %s %s', name, bin[name]);
+    });
+}
+
+function version() {
+    console.log('v' + require('../package').version);
+}
